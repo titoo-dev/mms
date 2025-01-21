18e8399f9465fc9496f5d9a6f9e334bf0b457095
Ony <onyrakoto27@gmail.com>
2025-01-21 17:01:01 +0300

    some cool things

--- src/server/schema/query/cover.ts
+++ src/server/schema/query/cover.ts
@@ -1,7 +1,8 @@
 import { builder } from "../builder.js";
 import { musicLibrary } from "src/lib/music-manager/music-manager.js";
+import { prisma } from "$prisma/client.js";
 
-builder.queryField("cover", (t) => {
+builder.queryField("coverFromPath", (t) => {
   return t.field({
     type: "String",
     args: {
@@ -9,6 +10,28 @@ builder.queryField("cover", (t) => {
     },
     resolve: async (_root, args) => {
       const coverBuffer = await musicLibrary.getCover(args.coverPath);
+      return coverBuffer.toString("base64");
+    },
+  });
+});
+
+builder.queryField("coverFromTrack", (t) => {
+  return t.field({
+    type: "String",
+    args: {
+      trackId: t.arg.id({ required: true }),
+    },
+    resolve: async (_root, args) => {
+      const track = await prisma.track.findUnique({
+        where: { id: args.trackId },
+        include: {
+          album: true,
+        },
+      });
+      if (!track) {
+        throw new Error("Track not found");
+      }
+      const coverBuffer = await musicLibrary.getCover(track.album.coverPath!);
       return coverBuffer.toString("base64");
     },
   });

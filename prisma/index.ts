7a6c92e7ac16640c079e55a09abc19b9fb5242eb
Ony <onyrakoto27@gmail.com>
2025-02-27 11:26:57 +0300

    refactor: update default API name in environment configuration

--- prisma/index.ts
+++ prisma/index.ts
@@ -3,7 +3,7 @@ import { BASE_CONFIG_DIR } from "../config";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-  datasourceUrl: `file:/${path.join(BASE_CONFIG_DIR, "database.db")}?connection_limit=1&socket_timeout=5`,
+  datasourceUrl: `file:${path.join(BASE_CONFIG_DIR, "database.db")}?connection_limit=1&socket_timeout=5`,
 });
 
 export enum StateKey {
